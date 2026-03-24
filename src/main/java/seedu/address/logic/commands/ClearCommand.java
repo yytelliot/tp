8902d00dce613b712d66ef21f80efa67dde9d85a
Shana <shanans264@gmail.com>
2026-03-24 16:58:58 +0800

    Add ConfirmCommand and ConfirmCommandParser

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,22 +2,21 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
 /**
- * Clears the address book.
+ * Prompts the user to confirm their intent to clear all OnlyTutors' contacts.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_CONFIRM = "This will delete all contacts. " +
+            "Type 'confirm clear' if you would still like to proceed.";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_CONFIRM);
     }
 }
