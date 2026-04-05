3d735402b2783876f4ae4719acbbf60c089b2641
Kim Yungju <yjkim101002@gmail.com>
2026-04-05 16:25:24 +0800

    Refactor MarkCommand to extend BatchCommand

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -1,11 +1,7 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
@@ -17,7 +13,7 @@ import seedu.address.model.person.Person;
 /**
  * Marks one or more students' payment status as paid.
  */
-public class MarkCommand extends Command {
+public class MarkCommand extends BatchCommand {
 
     public static final String COMMAND_WORD = "mark";
 
@@ -31,53 +27,39 @@ public class MarkCommand extends Command {
     public static final String MESSAGE_MARK_PERSONS_SUCCESS = "Marked %1$d students as paid: %2$s";
     public static final String MESSAGE_ALREADY_PAID = "This student has already been marked as paid.";
 
-    private final List<Index> targetIndices;
-
     /**
      * Creates a MarkCommand to mark the persons at {@code targetIndices} as paid.
      */
     public MarkCommand(List<Index> targetIndices) {
-        requireNonNull(targetIndices);
-        this.targetIndices = new ArrayList<>(targetIndices);
+        super(targetIndices);
     }
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
-
-        for (Index index : targetIndices) {
-            if (index.getZeroBased() >= lastShownList.size()) {
-                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-            }
-        }
-
-        List<Person> personsToMark = new ArrayList<>();
-        for (Index index : targetIndices) {
-            Person person = lastShownList.get(index.getZeroBased());
+    protected void checkPreconditions(List<Person> targetPersons) throws CommandException {
+        for (Person person : targetPersons) {
             if (person.isPaid()) {
                 throw new CommandException(MESSAGE_ALREADY_PAID);
             }
-            personsToMark.add(person);
         }
+    }
 
-        List<Person> markedPersons = new ArrayList<>();
-        for (Person personToMark : personsToMark) {
+    @Override
+    protected void executeBatch(List<Person> targetPersons, Model model) {
+        for (Person personToMark : targetPersons) {
             Person markedPerson = new Person(
                     personToMark.getName(), personToMark.getPhone(), personToMark.getEmail(),
                     personToMark.getAddress(), personToMark.getDay(), personToMark.getStartTime(),
                     personToMark.getEndTime(), personToMark.getRate(), true, personToMark.getTags());
             model.setPerson(personToMark, markedPerson);
-            markedPersons.add(markedPerson);
         }
+    }
 
-        if (markedPersons.size() == 1) {
-            return new CommandResult(String.format(MESSAGE_MARK_PERSON_SUCCESS,
-                    Messages.format(markedPersons.get(0))));
+    @Override
+    protected String formatSuccessMessage(List<Person> processedPersons) {
+        if (processedPersons.size() == 1) {
+            return String.format(MESSAGE_MARK_PERSON_SUCCESS, Messages.format(processedPersons.get(0)));
         }
-        String names = markedPersons.stream()
-                .map(p -> p.getName().toString()).collect(Collectors.joining(", "));
-        return new CommandResult(String.format(MESSAGE_MARK_PERSONS_SUCCESS, markedPersons.size(), names));
+        return String.format(MESSAGE_MARK_PERSONS_SUCCESS, processedPersons.size(), joinNames(processedPersons));
     }
 
     @Override
@@ -89,18 +71,18 @@ public class MarkCommand extends Command {
             return false;
         }
         MarkCommand otherMarkCommand = (MarkCommand) other;
-        return targetIndices.equals(otherMarkCommand.targetIndices);
+        return getTargetIndices().equals(otherMarkCommand.getTargetIndices());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(targetIndices);
+        return Objects.hash(getTargetIndices());
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("targetIndices", targetIndices)
+                .add("targetIndices", getTargetIndices())
                 .toString();
     }
 }
