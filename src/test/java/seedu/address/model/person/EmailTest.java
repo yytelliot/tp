d416cffb164045359508eb09ab6565aaa7c997bf
Shana <shanans264@gmail.com>
2026-04-07 20:13:03 +0800

    Add more tests

--- src/test/java/seedu/address/model/person/EmailTest.java
+++ src/test/java/seedu/address/model/person/EmailTest.java
@@ -50,7 +50,9 @@ public class EmailTest {
         assertFalse(Email.isValidEmail("peterjack@example.com.")); // domain name ends with a period
         assertFalse(Email.isValidEmail("peterjack@-example.com")); // domain name starts with a hyphen
         assertFalse(Email.isValidEmail("peterjack@example.com-")); // domain name ends with a hyphen
-        assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two chars
+        assertFalse(Email.isValidEmail("peterjack@example")); // domain name does not have a period
+        assertFalse(Email.isValidEmail("peterjack@example..com")); // domain name has two consecutive periods
+        assertFalse(Email.isValidEmail("peterjack@example.c")); // top level domain has less than two char
 
         // valid email
         assertTrue(Email.isValidEmail("PeterJack_1190@example.com")); // underscore in local part
